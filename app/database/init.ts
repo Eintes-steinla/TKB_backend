eff6712821cda080050a7b5a5c6ffd49f2facd65
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2026-06-09 10:02:23 +0700

    first

--- app/database/init.ts
+++ app/database/init.ts
@@ -1,12 +1,12 @@
-import { connectPostgres, disconnectPostgres } from './Postgres.database';
-import { connectRedis, disconnectRedis } from './Redis.database';
+import { connectPostgres, disconnectPostgres } from "./Postgres.database";
+import { connectRedis, disconnectRedis } from "./Redis.database";
 
 export async function initDatabases(): Promise<void> {
   await connectPostgres();
-  await connectRedis();
+  // await connectRedis();
 }
 
 export async function closeDatabases(): Promise<void> {
   await disconnectPostgres();
-  await disconnectRedis();
+  // await disconnectRedis();
 }
